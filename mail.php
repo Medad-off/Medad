0c33e91968cc174fc3b4fd4b7878aafdf2d45428
Medad-off <55643972+Medad-off@users.noreply.github.com>
2019-09-27 02:44:18 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -16,7 +16,21 @@ if ( $method === 'POST' ) {
 			";
 		}
 	}
-} 
+} else if ( $method === 'GET' ) {
+	$project_name = trim($_GET["project_name"]);
+	$admin_email  = trim($_GET["admin_email"]);
+	$form_subject = trim($_GET["form_subject"]);
+	foreach ( $_GET as $key => $value ) {
+		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+		}
+	}
+}
 $message = "<table style='width: 100%;'>$message</table>";
 function adopt($text) {
 	return '=?UTF-8?B?'.Base64_encode($text).'?=';
